79beab362dab655a99aaa978827733a5fb9b7107
shibamoto <silverbirder@gmail.com>
2024-03-23 20:14:28 +0900

    add debug

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     env: {
-      DATABASE_URL:
-        "mysql://test:test@localhost:3307/t3-app-nextjs-testcontainers",
+      // DATABASE_URL:
+      //   "mysql://test:test@localhost:3307/t3-app-nextjs-testcontainers",
     },
     alias: {
       "~": path.resolve(__dirname, "./src"),
