15c63cbc362ee411043038cb2a264fb8c460f815
shibamoto <silverbirder@gmail.com>
2024-03-24 21:45:49 +0900

    refactor: sample.test.ts

--- src/test/sample.test.ts
+++ src/test/sample.test.ts
@@ -1,6 +1,6 @@
 import { afterAll, beforeAll, describe, it, vi } from "vitest";
-import { MySqlContainer } from "@testcontainers/mysql";
-import { GenericContainer } from "testcontainers";
+import { MySqlContainer, StartedMySqlContainer } from "@testcontainers/mysql";
+import { GenericContainer, StartedTestContainer } from "testcontainers";
 import { createPool } from "mysql2";
 import { drizzle } from "drizzle-orm/mysql2";
 import { migrate } from "drizzle-orm/mysql2/migrator";
@@ -8,26 +8,45 @@ import { faker } from "@faker-js/faker";
 import * as schema from "../server/db/schema";
 import { type Browser, type Page, chromium } from "@playwright/test";
 
-describe("", () => {
-  vi.setConfig({ testTimeout: 600_000 });
+describe("App and Database Containers Integration Test", () => {
+  vi.setConfig({ testTimeout: 600_000, hookTimeout: 600_000 });
 
   let browser: Browser;
   let page: Page;
+  let appContainer: StartedTestContainer;
+  let mysqlContainer: StartedMySqlContainer;
+
   beforeAll(async () => {
-    browser = await chromium.launch({ headless: true });
+    // Browser setup
+    browser = await chromium.launch();
     page = await browser.newPage();
+
+    // Start MySQL container
+    mysqlContainer = await new MySqlContainer()
+      .withDatabase("t3-app-nextjs-testcontainers")
+      .start();
+
+    // Start application container
+    const innerDatabaseUrl = `mysql://${mysqlContainer.getUsername()}:${mysqlContainer.getUserPassword()}@${mysqlContainer.getIpAddress(mysqlContainer.getNetworkNames()[0] ?? "")}:3306/${mysqlContainer.getDatabase()}`;
+    const appImage = await GenericContainer.fromDockerfile("./")
+      .withBuildArgs({ NEXT_PUBLIC_CLIENTVAR: "clientvar" })
+      .withCache(true)
+      .build("app", { deleteOnExit: false });
+    appContainer = await appImage
+      .withEnvironment({ DATABASE_URL: innerDatabaseUrl, PORT: "3000" })
+      .withExposedPorts(3000)
+      .start();
   });
 
   afterAll(async () => {
+    // Stop containers and close browser
+    await appContainer.stop({ remove: false, removeVolumes: false });
+    await mysqlContainer.stop({ remove: false, removeVolumes: false });
     await browser?.close();
   });
 
-  it("", async () => {
-    const mysqlContainer = await new MySqlContainer()
-      .withDatabase("t3-app-nextjs-testcontainers")
-      .start();
+  it("should initialize the database and interact with the app through the browser", async () => {
     const databaseUrl = `mysql://${mysqlContainer.getUsername()}:${mysqlContainer.getUserPassword()}@${mysqlContainer.getHost()}:${mysqlContainer.getFirstMappedPort()}/${mysqlContainer.getDatabase()}`;
-    const innerDatabaseUrl = `mysql://${mysqlContainer.getUsername()}:${mysqlContainer.getUserPassword()}@${mysqlContainer.getIpAddress(mysqlContainer.getNetworkNames()[0] ?? "")}:3306/${mysqlContainer.getDatabase()}`;
     const db = drizzle(
       createPool({
         uri: databaseUrl,
@@ -38,6 +57,8 @@ describe("", () => {
       },
     );
     await migrate(db, { migrationsFolder: "src/server/db/out" });
+
+    // Insert mock data into the database
     const data: (typeof schema.posts.$inferInsert)[] = [];
     for (let i = 0; i < 20; i++) {
       data.push({
@@ -45,17 +66,9 @@ describe("", () => {
       });
     }
     await db.insert(schema.posts).values(data);
-    const appImage = await GenericContainer.fromDockerfile("./")
-      .withBuildArgs({ NEXT_PUBLIC_CLIENTVAR: "clientvar" })
-      .withCache(true)
-      .build("app", { deleteOnExit: false });
 
-    const appContainer = await appImage
-      .withEnvironment({ DATABASE_URL: innerDatabaseUrl, PORT: "3000" })
-      .withExposedPorts(3000)
-      .start();
+    // Interact with the app in the browser
     const url = `http://${appContainer.getHost()}:${appContainer.getFirstMappedPort()}`;
-    console.log({ url, databaseUrl, innerDatabaseUrl });
     await page.goto(url);
     await page.screenshot({ path: "screenshots/screenshot-1.png" });
     await page.getByPlaceholder("Title").fill("Hello World");
@@ -65,7 +78,5 @@ describe("", () => {
     await page.locator("button").isEnabled();
     await page.waitForSelector("text=Your most recent post: Hello World");
     await page.screenshot({ path: "screenshots/screenshot-4.png" });
-    await appContainer.stop({ remove: false, removeVolumes: false });
-    await mysqlContainer.stop({ remove: false, removeVolumes: false });
   });
 });
